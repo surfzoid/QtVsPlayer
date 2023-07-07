6b35590470ca6be6054a20443bcbf600c1bd1201
surfzoid <surfzoid@gmail.com>
2023-07-07 18:16:53 +0200

    git pish LP

---
+++ gitLP.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+SRC=/home/eric/Projets/qt/QtVsPlayer/QtVsPlayer.pro
+
+Version=$(grep "VERSION =" $SRC  | tac -s' ' | head -1)
+
+git tag $Version
+git push --tags git+ssh://surfzoid@git.launchpad.net/~surfzoid/+git/QtVsPlayer
+git push --tags 
