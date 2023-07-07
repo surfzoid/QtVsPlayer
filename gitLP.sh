75e8df928c711f6336442eaeacc2efb5935d46dd
surfzoid <surfzoid@gmail.com>
2023-07-07 19:50:01 +0200

    all in one push

--- gitLP.sh
+++ gitLP.sh
@@ -4,6 +4,6 @@ SRC=/home/eric/Projets/qt/QtVsPlayer/QtVsPlayer.pro
 Version=$(grep "VERSION =" $SRC  | tac -s' ' | head -1)
 
 git tag $Version
-git push git+ssh://surfzoid@git.launchpad.net/~surfzoid/+git/QtVsPlayer
-git push --tags git+ssh://surfzoid@git.launchpad.net/~surfzoid/+git/QtVsPlayer
+git push origin
+git push --tags origin
 #git push --tags 
